26da65d4a4ba4730bf3920466a8e0ced36683d32
David Cavalcanti <david_cavalcanti@outlook.com>
2024-08-30 10:47:22 -0300

    adicionando estilização aos componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Logo from './components/Logo/index.js'
+import './App.css';
+import Logo from './components/Logo/index.js';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <header className='App-header'>
         <Logo></Logo>
       </header>
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,13 +1,18 @@
-import './estilo.css'
-import logo from '../../image/logo.svg'
+import './estilo.css';
+import logo from '../../image/logo.svg';
 
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='Logo da livraria' />
+            <img
+                src={logo}
+                alt='Logo da livraria'
+                className='log-img'>
+            </img>
+
             <p>Mino<strong>Books</strong></p>
         </div>
     )
 }
 
-export default Logo
+export default Logo;
