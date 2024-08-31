1a2a858f47804ff9d94bdd217479f079635ec186
David Cavalcanti <david_cavalcanti@outlook.com>
2024-08-31 16:46:37 -0300

    Add lista de livros

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../image/livro.png'
+
+export const livros = [
+    { nome: 'JavaScript para iniciantes', id: 1, src: livro },
+    { nome: 'Liderança Técnica', id: 2, src: livro },
+    { nome: 'Banco de dados SQL', id: 3, src: livro },
+    { nome: 'TypeScript para iniciantes', id: 4, src: livro }
+]
