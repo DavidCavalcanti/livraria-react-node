6540d2786786bdfc17b6c52f103127cf767ea5c2
David Cavalcanti <david_cavalcanti@outlook.com>
2024-09-03 00:36:13 -0300

    Utilizando serviços

--- backend/src/controller/livroController.js
+++ backend/src/controller/livroController.js
@@ -1,14 +1,31 @@
-import fs from "fs";
+import { getLivroId, getTodosLivros } from "../services/livroServices.js";
 
 class LivroController {
 
     static listarTodosLivros(req, res) {
         try {
-            const buscarLivros = JSON.parse(fs.readFileSync("livros.json"));
+            const buscarLivros = getTodosLivros();
             res.status(200).json(buscarLivros)
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - Falha na requisição` });
         }
+    }
+
+    static buscaLivroId(req, res) {
+        try {
+            const livro = getLivroId(req.params.id);
+            console.log(livro);
+            if (livro.length !== 0) {
+                res.status(200).json({ message: "Livro encontrado!", livro });
+            } else {
+                res.status(404).json({ message: "Livro não encontrado! :-/" })
+            }
+
+        } catch (erro) {
+            res.status(500).json({ message: `Erro interno de servidor - ${erro.message}` });
+        }
+
+
     }
 
 }
