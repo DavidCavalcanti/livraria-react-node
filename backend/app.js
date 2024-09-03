354d96f1ce6b579f84f433c891bba95f5f1f2668
David Cavalcanti <david_cavalcanti@outlook.com>
2024-09-02 22:02:33 -0300

    Add ajustes para rotas de livros

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const routerLivros = require('./rotas/livro')
-const app = express()
-const port = 8000
-app.use('/livros', routerLivros)
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-} )
