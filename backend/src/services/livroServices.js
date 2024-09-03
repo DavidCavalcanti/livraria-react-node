7343c722aeb1cdc310d0f3957751978232ab5c7b
David Cavalcanti <david_cavalcanti@outlook.com>
2024-09-03 01:21:05 -0300

    Add serviço para criar novo livro

--- backend/src/services/livroServices.js
+++ backend/src/services/livroServices.js
@@ -3,6 +3,11 @@ import fs from "fs";
 export function getTodosLivros() { return JSON.parse(fs.readFileSync("livros.json")); }
 
 export function getLivroId(id) {
-    const livro = JSON.parse(fs.readFileSync("livros.json")).filter(livro => livro.id === id)
+    const livro = JSON.parse(fs.readFileSync("livros.json")).filter(livro => livro.id === id);
     return livro;
+}
+
+export function insertLivro(dados) {
+    const livros = getTodosLivros();
+    return fs.writeFileSync("livros.json", JSON.stringify([...livros, dados]));
 }
