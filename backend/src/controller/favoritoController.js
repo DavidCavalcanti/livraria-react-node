2489c2335e7f7ef8519aad4611df03bee5626d0f
David Cavalcanti <david_cavalcanti@outlook.com>
2024-09-16 23:46:52 -0300

    Adicionar controlador para inserir livro favorito

--- backend/src/controller/favoritoController.js
+++ backend/src/controller/favoritoController.js
@@ -1,4 +1,4 @@
-import { getTodosFavoritos } from "../services/favoritoServices.js";
+import { getTodosFavoritos, insertFavorito } from "../services/favoritoServices.js";
 
 class FavoritoController {
 
@@ -8,6 +8,22 @@ class FavoritoController {
             res.status(200).json(buscarFavoritos)
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - Falha na requisição` });
+        }
+    }
+
+    static adicionarFavorito(req, res) {
+        try {
+            const { id } = req.params;
+            console.log(id)
+            if (id >= 0 && id !== null) {
+                insertFavorito(id);
+                res.status(201).json({ message: "Livro favorito adicionado! ;-)" });
+            } else {
+                res.status(422).json({ message: "Id informado inválido!" });
+            }
+
+        } catch (erro) {
+            res.status(400).json({ message: `${erro.message}` });
         }
     }
 }
